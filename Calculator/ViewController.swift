b2b5908cab61bb6c25093c6f4ca6310d0e210fdc
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-11-05 20:41:00 +0900

    isFinishedTypingNumber の追加

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,25 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isFinishedTypingNumber: Bool = true
     @IBOutlet weak var displayLabel: UILabel!
-    
-    
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
     }
-
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle {
-            displayLabel.text = numValue
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
         }
-    
     }
-
 }
 
