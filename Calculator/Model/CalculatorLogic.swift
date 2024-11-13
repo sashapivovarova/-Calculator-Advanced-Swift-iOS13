fd9e850e044361daeb9d1d2959892c471aa0b568
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-11-13 20:53:42 +0900

    CalculatorLogic class の追加

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,5 +10,21 @@ import Foundation
 
 class CalculatorLogic {
     
+    var number: Double
     
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+        
+            if symbol == "+/-" {
+                return number * -1
+            } else if symbol == "AC" {
+                return 0
+            } else if symbol == "%" {
+                return number * 0.01
+            }
+        return nil
+    }
 }
