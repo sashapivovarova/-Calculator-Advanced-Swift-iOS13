ee0cb1ebdb9851ed318843125700589be0e786eb
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-11-17 19:22:53 +0900

    performTwoNumCalculation の追加

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,11 +12,13 @@ struct CalculatorLogic {
     
     private var number: Double?
     
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
             if symbol == "+/-" {
                 return n * -1
@@ -24,6 +26,28 @@ struct CalculatorLogic {
                 return 0
             } else if symbol == "%" {
                 return n * 0.01
+            } else if symbol == "=" {
+                return performTwoNumCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
             }
         }
         return nil
